59d01c5e3e66df085eebbcb91b904189352b239d
greenT-Hee <heehee970322@gmail.com>
2022-09-05 18:40:09 +0900

    Error:배포 빈화면

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,4 +1,4 @@
-import { Route, BrowserRouter, Routes } from "react-router-dom";
+import { Route, BrowserRouter, Routes, HashRouter } from "react-router-dom";
 import { ThemeProvider } from "styled-components";
 import "../src/styles/reset.css";
 import "../src/styles/global.css";
@@ -26,7 +26,7 @@ const theme = {
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <HashRouter basename={window.location.pathname || ""}>
         <Routes>
           <Route exact path="/" element={<Home />} />
           <Route path="/login" element={<LoginPage />} />
@@ -36,7 +36,7 @@ function App() {
           <Route path="/payedList" element={<PayedCard />} />
           <Route path="/payedDetail/:order_num" element={<PayedDetailPage />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </ThemeProvider>
   );
 }
